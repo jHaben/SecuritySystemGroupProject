b9d297f77bef188b140d770647a5bb9e9a2bb105
jHaben <jrhaben@gmail.com>
2021-04-23 10:33:50 -0500

    AwayStage
    1 handle event door open
    
    2 handle event movement

--- src/states/AwayStage.java
+++ src/states/AwayStage.java
@@ -1,19 +1,20 @@
 package states;
 
-import events.AllDoorCloseEvent;
-import events.CancelPressEvent;
+import events.DoorOpensEvent;
+import events.MovementEvent;
 
 public class AwayStage extends SecuritySystemState {
 	private static AwayStage instance;
-	
+
 	/**
 	 * Private constructor. Singleton.
 	 */
 	private AwayStage() {
 	}
-	
+
 	/**
 	 * returning the instance
+	 * 
 	 * @return the instance object
 	 */
 	public static AwayStage instance() {
@@ -22,20 +23,36 @@ public class AwayStage extends SecuritySystemState {
 		}
 		return instance;
 	}
-	
+
 	/**
-	 * Entering away-armed state. 
-	 * Will display on the GUI
+	 * Entering away-armed state. Will display on the GUI
 	 */
 	@Override
 	public void enter() {
 		SecuritySystemContext.instance().showAwayArmed();
 	}
 
+	/**
+	 * @param event Process when the door opens.
+	 */
+	@Override
+	public void handleEvent(DoorOpensEvent event) {
+		SecuritySystemContext.instance().changeState(WarningStage.instance());
+	}
+
+	/**
+	 * @param event
+	 * @see states.SecuritySystemState#handleEvent(events.MovementEvent)
+	 * 
+	 *      Process when there is movement.
+	 */
+	@Override
+	public void handleEvent(MovementEvent event) {
+		SecuritySystemContext.instance().changeState(WarningStage.instance());
+	}
+
 	@Override
 	public void leave() {
 	}
-	
-	
-	
+
 }
