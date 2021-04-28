885102977b77eeb1be56c01534efd9a0b7cd4ea4
Kou <Kou@KY>
2021-04-28 16:20:03 -0500

    Edit NumberButton class to allow button inputs for Breach States

--- src/buttons/NumberButton.java
+++ src/buttons/NumberButton.java
@@ -33,7 +33,8 @@ public class NumberButton extends GUIButton implements EventHandler<ActionEvent>
 	public void handle(ActionEvent event) {
 		String currentStateString = SecuritySystemContext.instance().getCurrentState().getClass().getSimpleName();
 		if(currentStateString.equals("PasswordRequiredStage") || currentStateString.equals("WarningDoorsClosedState")
-				|| currentStateString.equals("WarningDoorsOpenState") || currentStateString.equals("BreachStage")) {
+				|| currentStateString.equals("WarningDoorsOpenState") || currentStateString.equals("BreachDoorsOpenStage")
+				|| currentStateString.equals("BreachDoorsClosedState")) {
 			GUIDisplay.getInstance().clearText();
 			GUIDisplay.getInstance().showingPassword(numberButtonValue);
 			SecuritySystemContext.instance().handleEvent(ValidPassEvent.instance());
